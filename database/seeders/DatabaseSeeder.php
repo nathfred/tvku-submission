8ff7e3e94fe39cd9703966ec4441168f3e84baf7
Nathanael Fredericko <nathanaelfredericko@gmail.com>
2021-09-10 15:20:47 +0700

    Setup DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Employee;
+use App\Models\Submission;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create('id_ID');
+
+        // ADMIN
+        User::create([
+            'name' => 'Admin TVKU',
+            'role' => 'admin',
+            'ktp' => $faker->nik(),
+            'address' => 'Jl. Nakula 1 No. 5-11, Semarang',
+            'birth' => '2000-08-25',
+            'last_education' => 'Universitas TVKU',
+            'phone' => '08123456789',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('123123'), // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        // USER
+        User::factory(env('SAMPLE_USER', 10))->create();
+
+        // EMPLOYEE
+        Employee::factory(env('SAMPLE_USER', 10))->create();
+
+        // SUBMISSION
+        Submission::factory(5)->create();
     }
 }
